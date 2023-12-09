ea0133131f279b16b88d7a4c55fe510b753a8eaf
thiagosnx <thiagosantanacq@gmail.com>
2023-12-09 13:46:25 -0300

    comentarios

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -7,6 +7,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { environment } from '../../../../enviroments/enviroment';
 import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { MessagesService } from '../../../services/messages.service';
+import { Comment } from '../../../Comment';
+import { CommentService } from '../../../services/comment.service';
+import { FormGroup, FormControl, Validators, FormGroupDirective } from '@angular/forms';
 
 @Component({
   selector: 'app-moment',
@@ -21,15 +24,32 @@ export class MomentComponent {
   faTimes = faTimes;
   faEdit = faEdit;
 
+  commentForm!:FormGroup;
+
+
   constructor(private momentService: MomentService,
      private route: ActivatedRoute, 
      private messagesService: MessagesService,
-     private router: Router){  }
+     private router: Router,
+     private commentService: CommentService){  }
 
   ngOnInit():void{
     const id = Number(this.route.snapshot.paramMap.get('id')); //recuperando o id da url
 
     this.momentService.getMoment(id).subscribe((item) => (this.moment = item.data));
+
+    this.commentForm = new FormGroup({
+      text: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get text(){
+    return this.commentForm.get('text')!;
+  }
+
+  get username(){
+    return this.commentForm.get('username')!;
   }
 
   async removeHandler(id:number){
@@ -39,5 +59,25 @@ export class MomentComponent {
 
     this.router.navigate(['/']);
   }
+
+  async onSubmtit(formDirective: FormGroupDirective){ 
+    if(this.commentForm.invalid){
+      return
+    }
+
+    const data: Comment = this.commentForm.value
+
+    data.momentId = Number(this.moment!.id)
+
+    await this.commentService.createComment(data).subscribe((comment) =>
+      this.moment!.comments!.push(comment.data));
+
+    this.messagesService.add('Successfully!')
+
+    this.commentForm.reset(); //limpa o formulario no front
+    formDirective.resetForm(); //limpa o formulario pro back
+
+
+   }
 
 }
